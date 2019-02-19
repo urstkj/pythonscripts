40b1562c9e95acc208c6dcc0bc839f9f05ad069e
urstkj <toleshkumar1998@gmail.com>
2019-02-19 23:11:54 +0530

    Update otpsender.py

--- otpsender.py
+++ otpsender.py
@@ -3,7 +3,7 @@ import json
 from time import sleep
 i = 1
 while True:
-    otp = requests.get('https://crg.delhivery.com/otp/generate?phone=7014683236')
+    otp = requests.get('api_url')
     otpstatus = json.loads(otp.content.decode('utf-8'))
     print(i)
     print(otpstatus['message'])
